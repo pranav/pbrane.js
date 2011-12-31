003db7db07c6644cce06f003a5f433a13d5e297e
Pranav Gandhi <pranavrulz@gmail.com>
2011-12-30 20:14:30 -0500

    Added Drawing to Makefile and started work on drawRect and drawCircle

--- Makefile
+++ Makefile
@@ -5,6 +5,7 @@ run:
 	touch build/pbrain.js
 	cat pbrane.js >> build/pbrain.js
 	cat util/Vector2D.js >> build/pbrain.js
+	cat util/Drawing.js >> build/pbrain.js
 	echo 'Build all done. Located at build/pbrain.js'
 
 clean: 
---
+++ util/Drawing.js
@@ -0,0 +1,21 @@
+/** Drawing library for pbrain.js */
+
+
+
+/** drawRect : Int Int Int Int String Scene
+ * drawRect requires the x, y, width, height, color (in hex), and Context
+ * It will use the built in scn.fillRect */
+function drawRect(x, y, width, height, color, scn){
+    scn.fillStyle = color;
+    scn.fillRect(x, y, x + width, y + height);
+    return scn;
+}
+
+/** drawCircle : Int Int Int Color Scene
+ * drawCircle requires x, y, radius, color (in hex), and Context */
+function drawCircle(x, y, r, color, scn){
+    scn.beginpath();
+    scn.arc(x, y, r, 0, 2*Math.PI, false);
+    scn.fillStyle = color;
+    scn.fill();
+}
