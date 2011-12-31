35e8ed0987311c5272814b41051658172740b209
Pranav Gandhi <pranavrulz@gmail.com>
2011-12-30 20:43:37 -0500

    Added output contract

--- util/Drawing.js
+++ util/Drawing.js
@@ -2,7 +2,7 @@
 
 
 
-/** drawRect : Int Int Int Int String Scene
+/** drawRect : Int Int Int Int String Scene -> Scene
  * drawRect requires the x, y, width, height, color (in hex), and Context
  * It will use the built in scn.fillRect */
 function drawRect(x, y, width, height, color, scn){
@@ -11,11 +11,12 @@ function drawRect(x, y, width, height, color, scn){
     return scn;
 }
 
-/** drawCircle : Int Int Int Color Scene
+/** drawCircle : Int Int Int Color Scene -> Scene
  * drawCircle requires x, y, radius, color (in hex), and Context */
 function drawCircle(x, y, r, color, scn){
     scn.beginpath();
     scn.arc(x, y, r, 0, 2*Math.PI, false);
     scn.fillStyle = color;
     scn.fill();
+    return scn;
 }
